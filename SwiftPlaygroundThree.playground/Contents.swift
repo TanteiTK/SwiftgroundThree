6a6b7f750fd47b7960b453d9d354962aff60c150
CM Student <cmstudent@UHWO-ACM-C-01.local>
2019-02-14 10:30:10 -1000

    Was able to do raise and index completed 2/14/2019

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//part2
+//declaration
+var fruitNames:[String]
+//assign fruitNames with 3 of your favorite fruits
+fruitNames = ["Peach", "Lychee", "Fuji Pears"]
+//part4
+print (fruitNames)
+
+//string interpolation, this is not an irration
+print(" I like to eat/fruitNames)")
+
+
+for name in fruitNames
+{
+    print ("I like to eat" + name )
+}
+
+//part7 #2
+//print "name" at index 1 in fruitNames
+print(fruitNames[1])
+
+print(fruitNames[0])
